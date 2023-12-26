d46136a59e9654756f267c81755ffad752e2c176
Mohammed Alawad <50057882+malawadd@users.noreply.github.com>
2023-12-26 19:37:57 +0300

    SimpleStorage

---
+++ SimpleStorage.sol
@@ -0,0 +1,40 @@
+// تعليق
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19; //إصدار سوليتدي 
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
+
+contract SimpleStorage {
+    uint256 myFavoriteNumber;
+
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
+
+    // khaled => 77
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        myFavoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
+
+   function retrivePure() public pure returns(uint256) {
+          return 7 ; 
+      }
+
+    // calldata , memory, storage
+    // array, struct or mapping 
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
